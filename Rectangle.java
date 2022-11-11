b3ac27f7ed5fa399df2da89de1f93dd0e0f46c46
Mangofish369 <117941655+Mangofish369@users.noreply.github.com>
2022-11-11 14:29:47 -0500

    Commented Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,14 +1,17 @@
 public class Rectangle
 {
+    // instance variables
     private int width;
     private int height;
     
+    //constructors
     public Rectangle(int rectWidth, int rectHeight)
     {
         width = rectWidth;
         height = rectHeight;
     }
     
+    //Instance method
     public int getArea()
     {
         return width * height;
@@ -24,6 +27,7 @@ public class Rectangle
         return width;
     }
     
+    //String return
     public String toString()
     {
         return "Rectangle with width: " + width + " and height: " + height;
